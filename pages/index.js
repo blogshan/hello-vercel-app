de7cfd97aa29a6eccf7b3e8922467d09c5fd5447
blogshan <cdndesh@gmail.com>
2026-01-10 02:13:43 +0530

    Add initial Home component with styling

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+export default function Home() {
+  return (
+    <div style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "100vh",
+      fontFamily: "Arial",
+      backgroundColor: "#0f172a",
+      color: "white",
+      textAlign: "center"
+    }}>
+      <div>
+        <h1>Hello Vercel 🚀</h1>
+        <p>My first app is live!</p>
+      </div>
+    </div>
+  );
+}
